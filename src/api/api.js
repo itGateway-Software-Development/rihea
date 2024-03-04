c78c04c97f482d30c01a6d31945cc88ffb133818
itGateway-Software-Development <developer@itgateway.org>
2024-03-04 15:39:33 +0630

    api update

--- src/api/api.js
+++ src/api/api.js
@@ -1,10 +1,10 @@
-let host = 'https://www.admin.rihea.education/api/v1/';
+let host = 'https://admin.rihea.education/api/v1/';
 
 let api = {
     getBlogs: host + 'blogs',
     getBlog: host + 'blogs/',
     topBlogs: host + 'topBlogs',
-    photo_url: 'https://www.admin.rihea.education/storage/images/'
+    photo_url: 'https://admin.rihea.education/storage/images/'
 };
 
 export default api;
